005ea3f509142bba9ffb1e4d44788ef9bba288eb
koterin <katerine.grin@gmail.com>
2023-07-09 17:27:55 +0400

    remove slice

--- medium/most-water/main.go
+++ medium/most-water/main.go
@@ -11,33 +11,24 @@ func main() {
 }
 
 func findContainer(m []int) int {
-	containers := []int{}
+	area := 0
 
 	for i := 0; i < len(m); i++ {
 		for j := i + 1; j < len(m); j++ {
-			fmt.Println("checking m[", i, "]=", m[i], " and m[", j, "]=", m[j])
-
 			a := getArea(i, m[i], j, m[j])
-			containers = append(containers, a)
-
-			fmt.Println("got area = ", a)
+			if a > area {
+				area = a
+			}
 		}
 	}
 
-	fmt.Println("resulting containers array: ", containers)
-
-	if len(containers) < 1 {
-		return 0
-	}
-
-	return findMax(containers)
+	return area
 }
 
 func getArea(i int, v1 int, j int, v2 int) int {
-	x := 0
 	y := 0
 
-	x = j - i
+	x := j - i
 
 	if v2 > v1 {
 		y = v1
@@ -46,16 +37,4 @@ func getArea(i int, v1 int, j int, v2 int) int {
 	}
 
 	return x * y
-}
-
-func findMax(c []int) int {
-	max := c[0]
-
-	for _, n := range c {
-		if n > max {
-			max = n
-		}
-	}
-
-	return max
 }
